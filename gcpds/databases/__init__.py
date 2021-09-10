6dda566a63eedab32561b9c815377c8396b63f64
Yeison Cardona <yencardonaal@unal.edu.co>
2021-09-10 17:50:58 -0500

    import from base

--- gcpds/databases/__init__.py
+++ gcpds/databases/__init__.py
@@ -1,1 +1,18 @@
- 
+from . import Auditory_processing
+from . import BCI_Competition_IV
+from . import DUMMY
+from . import GIGA_BCI
+from . import GIGA_MI_ME
+from . import HighGamma_ME
+from . import PhysioNet_MI_ME
+
+available_databases = [
+    'Auditory_processing',
+    'BCI_Competition_IV.Dataset_2a',
+    'DUMMY',
+    'GIGA_BCI',
+    'GIGA_MI_ME',
+    'HighGamma_ME',
+    'PhysioNet_MI_ME',
+]
+
